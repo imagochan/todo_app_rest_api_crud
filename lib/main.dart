ce31f82cd1939438754577665bbaa7130f3dbdda
Julio <julioalfredomachado@gmail.com>
2023-08-17 03:13:21 -0600

    built basic ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_rest_api_crud/screens/todo_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: TodoListPage(),
+    );
   }
 }
